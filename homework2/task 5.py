f9f755a8cb00ef0c3ca1bcd72a8633f0b1e4d0af
Liza Sheresheva <101921398+sheresheva2001@users.noreply.github.com>
2022-10-04 21:02:31 +0300

    Initial commit

---
+++ homework2/task 5.py
@@ -0,0 +1,6 @@
+# Замените все пробелы в строке на точки на используя replace.
+
+a=str(input('введите строку:'))
+list=a.split()
+list1='.'.join(list)
+print(list1)
---
+++ homework2/task 6.py
@@ -0,0 +1,8 @@
+# Вводится дата в формате dd.mm.yyy.
+# Вывести дату в формате mm\dd\yyyy.
+
+a=str(input('введите дату в формате dd.mm.yyy:'))
+list=a.replace('.',' \ ')
+print(list)
+
+
